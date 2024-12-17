cbe3fa48daba2e438f2ce0f10e188715aa4f312f
Naufal Sandy <naufalsandyp@gmail.com>
2024-12-17 12:36:46 +0700

    ubah method

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
+const db= require('./connection')
 app.use(bodyParser.json())
 
 // routes/ URL/ENDPOINT utama dengan method GET
 app.get('/', (req, res) => {
   res.send('halaman UTAMA')
 })
-app.get('/hello', (req, res) => {
-  res.send('Hello World!')
-})
+
 //method POST
 app.post('/login', (req, res) => {
     console.log({requestFromOutside: req.body});    
